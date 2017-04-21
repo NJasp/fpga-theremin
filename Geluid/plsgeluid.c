b4142701ea22f24d75918bd2f14d65e63e283eee
Nils Jaspers <cat@national.shitposting.agency>
2017-04-21 12:08:01 +0200

    added an attempt at working audio code

---
+++ Geluid/plsgeluid.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <math.h>
+#include "includes.h"
+#include "altera_up_avalon_audio.h"
+
+/* Definition of Task Stacks */
+#define   TASK_STACKSIZE       2048
+OS_STK    taskSound_stk[TASK_STACKSIZE];
+OS_STK    task2_stk[TASK_STACKSIZE];
+int freq = 1;
+
+/* Definition of Task Priorities */
+
+#define TASK1_PRIORITY      1
+#define TASK2_PRIORITY      2
+#define AMP_VAL				100
+
+alt_up_audio_dev *audio_dev;
+
+void taskSound(void* pdata) {
+    int buf[62];
+    double i = 0;
+    int c = 0;
+
+    while(i<2*M_PI) {
+        buf[c] = (int) sin(i*freq)*AMP_VAL;
+        i+=.1;
+        c++;
+    }
+	// write audio to main speakers
+	alt_up_audio_play_l(audio_dev, &(buf), 10);
+	alt_up_audio_play_r(audio_dev, &(buf), 10);
+
+	// write audio buffer
+	alt_up_audio_write_fifo(audio_dev, &(buf), 1, ALT_UP_AUDIO_RIGHT);
+	alt_up_audio_write_fifo(audio_dev, &(buf), 1, ALT_UP_AUDIO_LEFT);
+
+	OSTimeDlyHMSM(0, 0, 0, 5);
+}
+/* Prints "Hello World" and sleeps for three seconds */
+void task2(void* pdata) {
+	for(;;) {
+		OSTimeDlyHMSM(0, 0, 10, 0);
+		freq++;
+		printf("incremented frequency\n");
+	}
+}
+/* The main function creates two task and starts multi-tasking */
+int main(void) {
+	audio_dev = alt_up_audio_open_dev ("/dev/Audio_Subsystem_Audio");
+	if ( audio_dev == NULL)
+	{
+		alt_printf ("Error: could not open audio device\n");
+		return -1;
+	}
+	else
+		alt_printf ("Opened audio device\n");
+
+  OSTaskCreateExt(taskSound, NULL, (void *)&taskSound_stk[TASK_STACKSIZE-1],
+                  TASK1_PRIORITY, TASK1_PRIORITY, taskSound_stk, TASK_STACKSIZE, NULL, 0);
+               
+  OSTaskCreateExt(task2, NULL, (void *)&task2_stk[TASK_STACKSIZE-1],
+                  TASK2_PRIORITY, TASK2_PRIORITY, task2_stk, TASK_STACKSIZE, NULL, 0);
+  OSStart();
+  return 0;
+}
