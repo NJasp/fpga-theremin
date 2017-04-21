ac9dded62afd2ef8d7dd48a3e834fc5140242552
Nils Jaspers <cat@national.shitposting.agency>
2017-04-21 13:58:00 +0200

    there was an attempt at audio

--- Geluid/plsgeluid.c
+++ Geluid/plsgeluid.c
@@ -6,13 +6,10 @@
 /* Definition of Task Stacks */
 #define   TASK_STACKSIZE       2048
 OS_STK    taskSound_stk[TASK_STACKSIZE];
-OS_STK    task2_stk[TASK_STACKSIZE];
 int freq = 1;
 
 /* Definition of Task Priorities */
-
 #define TASK1_PRIORITY      1
-#define TASK2_PRIORITY      2
 #define AMP_VAL				1000
 
 alt_up_audio_dev *audio_dev;
@@ -22,26 +19,21 @@ void taskSound(void* pdata) {
     double i = 0;
 
     for(;;) {
+    	int words = alt_up_audio_write_fifo_space(audio_dev, ALT_UP_AUDIO_RIGHT);
+    	alt_up_audio_write_fifo_space(audio_dev, ALT_UP_AUDIO_RIGHT);
 		while(i<2*M_PI) {
 			buf = (int) sin(i*freq)*AMP_VAL;
-			alt_up_audio_play_l(audio_dev, &buf, 10);
-			alt_up_audio_play_r(audio_dev, &buf, 10);
-			alt_up_audio_write_fifo(audio_dev, &buf, 1, ALT_UP_AUDIO_RIGHT);
-			alt_up_audio_write_fifo(audio_dev, &buf, 1, ALT_UP_AUDIO_LEFT);
 			i+=.1;
+            alt_up_audio_write_fifo(audio_dev, &buf, --words, ALT_UP_AUDIO_RIGHT);
+            alt_up_audio_write_fifo(audio_dev, &buf, words, ALT_UP_AUDIO_LEFT);
+            alt_up_audio_play_r(audio_dev, &buf, words);
+            alt_up_audio_play_l(audio_dev, &buf, words);
 		}
-
+        
 		OSTimeDlyHMSM(0, 0, 0, 5);
     }
 }
-/* Prints "Hello World" and sleeps for three seconds */
-void task2(void* pdata) {
-	for(;;) {
-		OSTimeDlyHMSM(0, 0, 10, 0);
-		freq++;
-		printf("incremented frequency\n");
-	}
-}
+
 /* The main function creates two task and starts multi-tasking */
 int main(void) {
 	audio_dev = alt_up_audio_open_dev ("/dev/Audio_Subsystem_Audio");
@@ -54,9 +46,7 @@ int main(void) {
 
   OSTaskCreateExt(taskSound, NULL, (void *)&taskSound_stk[TASK_STACKSIZE-1],
                   TASK1_PRIORITY, TASK1_PRIORITY, taskSound_stk, TASK_STACKSIZE, NULL, 0);
-               
-  OSTaskCreateExt(task2, NULL, (void *)&task2_stk[TASK_STACKSIZE-1],
-                  TASK2_PRIORITY, TASK2_PRIORITY, task2_stk, TASK_STACKSIZE, NULL, 0);
+
   OSStart();
   return 0;
 }
